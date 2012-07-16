8a958e7d139fbbd06f3967165bab33041ce94e54
Rodrigo Manhães <rmanhaes@gmail.com>
2012-07-16 13:31:54 -0300

    crafting rails applications, chapter 1

---
+++ crafting-rails-application/01/pdf_renderer/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '73fdf96cdee2331609904875ea05d5740903054cb7d995c60420cc4177b8de9b5dd89bd9c674d670d18abf0174cec951968d77bb818b0d64d586107c26ea5151'
---
+++ crafting-rails-application/01/pdf_renderer/test/integration/navigation_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class NavigationTest < ActiveSupport::IntegrationCase
+  test "pdf request sends a pdf as file" do
+    visit home_path
+    click_link 'PDF'
+
+    assert_equal 'binary', headers['Content-Transfer-Encoding']
+    assert_equal 'attachment; filename="contents.pdf"',
+      headers['Content-Disposition']
+    assert_equal 'application/pdf', headers['Content-Type']
+    # http://forums.pragprog.com/forums/191/topics/10282
+    assert_match /Prawn/, page.source
+  end
+
+  test 'pdf renderer uses the specified template' do
+    visit '/another.pdf'
+    assert_equal 'binary', headers['Content-Transfer-Encoding']
+    assert_equal 'attachment; filename="contents.pdf"',
+      headers['Content-Disposition']
+    assert_equal 'application/pdf', headers['Content-Type']
+    # http://forums.pragprog.com/forums/191/topics/10282
+    assert_match /Prawn/, page.source
+  end
+
+  protected
+
+  def headers
+    page.response_headers
+  end
+end
