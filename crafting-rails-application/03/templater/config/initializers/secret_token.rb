3547fa6f27a5eda26f7adda39603bea279a49bb5
Rodrigo Manhães <rmanhaes@gmail.com>
2012-07-23 02:03:22 -0300

    crafting rails applications, chapter 3, sections 3.1 and 3.2

---
+++ crafting-rails-application/03/templater/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Templater::Application.config.secret_token = '5f4e410a0977bd8a89546ef243806b17a40fcdf1de714fd2c78384a9802678cc1701887c85de1b78888764d251a2458f4b7c355eb27a6ef2586a6c55d029eada'
