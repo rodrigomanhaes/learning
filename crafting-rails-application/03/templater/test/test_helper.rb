a21298c075245546f69301af2a1a43e8e21e8daa
Rodrigo Manhães <rmanhaes@gmail.com>
2012-07-23 21:44:03 -0300

    crafting rails applications, chapter 3, section 3.3

--- crafting-rails-application/03/templater/test/test_helper.rb
+++ crafting-rails-application/03/templater/test/test_helper.rb
@@ -1,6 +1,10 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'capybara/rails'
+
+Rails.backtrace_cleaner.remove_silencers!
+Capybara.default_driver = :rack_test
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
