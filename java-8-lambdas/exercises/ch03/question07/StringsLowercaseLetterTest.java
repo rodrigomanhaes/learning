914943c1cc7556ea2d0682ffd6b6a2bbe2cc048a
Rodrigo Manhães <rmanhaes@gmail.com>
2014-05-08 16:46:09 -0300

    java 8 lambdas, chapter 3, exercise 7

---
+++ java-8-lambdas/exercises/ch03/question07/StringsLowercaseLetterTest.java
@@ -0,0 +1,13 @@
+package question07;
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class StringsLowercaseLetterTest {
+    @Test
+    public void findsStringWithLargestNumberOfLowercaseLetters() {
+        assertEquals("nadanao", StringsLowercaseLetter.find(
+                asList("teste", "coisaLOUCa", "nadanao", "algo assIM")));
+    }
+}
