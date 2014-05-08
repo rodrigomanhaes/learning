430a1cf483c9d843b0987f0e045f306f7374d31a
Rodrigo Manhães <rmanhaes@gmail.com>
2014-05-08 16:30:34 -0300

    java 8 lambdas, chapter 3, exercise 6

---
+++ java-8-lambdas/exercises/ch03/question06/LowercaseLettersCounter.java
@@ -0,0 +1,9 @@
+package question06;
+
+public class LowercaseLettersCounter {
+    public static long count(final String string) {
+        return string.chars()
+                .filter(c -> Character.isLowerCase(c))
+                .count();
+    }
+}
